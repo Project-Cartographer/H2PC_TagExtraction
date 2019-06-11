0600328297ff22d1ff6fb95d4619c0a90c3d721c
Himanshu-01 <kujurhimanshu66@gmail.com>
2019-06-12 03:23:19 +0530

    Fixed a simple directory issue and readded the Asset checks.

--- BlamLib/BlamLib.Test/Halo2/Extraction.cs
+++ BlamLib/BlamLib.Test/Halo2/Extraction.cs
@@ -251,7 +251,10 @@ namespace BlamLib.Test
                 kMapsDirectoryPc + @"mainmenu.map",
                 kMapsDirectoryPc + @"shared.map",
                 kMapsDirectoryPc + @"single_player_shared.map");
-            
+            Assert.IsNotNull(Program.Halo2.PcMainmenu);
+            Assert.IsNotNull(Program.Halo2.PcShared);
+            Assert.IsNotNull(Program.Halo2.PcCampaign);
+
 
             (Program.GetManager(game) as Managers.IStringIdController).StringIdCacheOpen(game);
             (Program.GetManager(game) as Managers.IVertexBufferController)
